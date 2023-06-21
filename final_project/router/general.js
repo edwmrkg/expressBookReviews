937e04179165f24b5ef805930c80b42705419956
magrigore <magrigore@bitdefender.com>
2023-06-21 21:24:07 +0300

    task 10-14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,56 +21,91 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
+// public_users.get('/',function (req, res) {
+//   return res.status(200).json(books);
+// });
 public_users.get('/',function (req, res) {
-  return res.status(200).json(books);
+  let promise = new Promise((resolve, reject) => {
+    resolve(books);
+  })
+  promise.then((books) => {
+    return res.status(200).json(books);
+  })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  if (!isbn) {
-    return res.status(404).json({message: "Invalid parameter"});
-  }
-  const book = books[isbn];
-  if (!book) {
-    return res.status(404).json({message: `Book with ISBN ${isbn} not found`});
-  }
+  let promise = new Promise((resolve, reject) => {
+    const isbn = req.params.isbn;
+    if (!isbn) {
+      reject("Invalid parameter");
+    }
+    const book = books[isbn];
+    if (!book) {
+      reject(`Book with ISBN ${isbn} not found`);
+    }
+    resolve(book);
+  })
 
-  return res.status(200).json(book);
- });
+  promise
+    .then((book) => {
+      return res.status(200).json(book);
+    })
+    .catch((error) => {
+      return res.status(404).json({message: error});
+    })
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  if (!author) {
-    return res.status(404).json({message: "Invalid parameter"});
-  }
-
-  let books_by_author = {};
-  for (isbn in books) {
-    if (books[isbn].author === author) {
-      books_by_author[isbn] = books[isbn];
+  let promise = new Promise((resolve, reject) => {
+    const author = req.params.author;
+    if (!author) {
+      reject("Invalid parameter");
     }
-  }
 
-  return res.status(200).json(books_by_author);
+    let books_by_author = {};
+    for (isbn in books) {
+      if (books[isbn].author === author) {
+        books_by_author[isbn] = books[isbn];
+      }
+    }
+    resolve(books_by_author);
+  });
+
+  promise
+    .then((books) => {
+      return res.status(200).json(books);
+    })
+    .catch((error) => {
+      return res.status(404).json({message: error});
+    })
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  if (!title) {
-    return res.status(404).json({message: "Invalid parameter"});
-  }
-
-  let books_by_title = {};
-  for (isbn in books) {
-    if (books[isbn].title === title) {
-      books_by_title[isbn] = books[isbn];
+  let promise = new Promise((resolve, reject) => {
+    const title = req.params.title;
+    if (!title) {
+      reject("Invalid parameter");
     }
-  }
 
-  return res.status(200).json(books_by_title);
+    let books_by_title = {};
+    for (isbn in books) {
+      if (books[isbn].title === title) {
+        books_by_title[isbn] = books[isbn];
+      }
+    }
+    resolve(books_by_title);
+  });
+
+  promise
+    .then((books) => {
+      return res.status(200).json(books);
+    })
+    .catch((error) => {
+      return res.status(404).json({message: error});
+    })
 });
 
 //  Get book review
